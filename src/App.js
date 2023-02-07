e4cc4a7ef404cdd92de016dd575ef15d472af439
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-02-07 22:44:49 +0400

    Set up routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import Home from "./routes/Home"
+import Products from "./routes/Products"
+import Basket from "./routes/Basket"
+import NoMatch from "./routes/NoMatch"
+import {Routes, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+    <Route index element={<Home />} />
+    <Route path="/" element={<Home />} />
+    <Route path="products" element={<Products />}/>
+    <Route path="basket" element={<Basket />}/>
+    <Route path='*' element={<NoMatch/>}/>
+    </Routes>
   );
 }
 
