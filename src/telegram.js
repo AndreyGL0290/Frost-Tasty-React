0f6f4f9e21798c548fd0bf0acebcb04943a7574a
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-02-12 20:56:32 +0400

    Telegram components set up

---
+++ src/telegram.js
@@ -0,0 +1,20 @@
+import basket from './basket'
+
+let tg = window.Telegram.WebApp
+
+tg.MainButton.color = '#FFBF00'
+tg.MainButton.textColor = '#000000'
+
+tg.MainButton.onClick(() => {
+    window.localStorage.clear()
+    let data = {}
+
+    // Shortens basket product data so only valuable information is send
+    for (let i = 0; i < Object.keys(basket.products).length; i += 1){
+        data[Object.keys(basket.products)[i]] = {'price': basket.products[Object.keys(basket.products)[i]].price, 'quantity': basket.products[Object.keys(basket.products)[i]].quantity}
+    }
+
+    tg.sendData(JSON.stringify(data))
+})
+
+export default tg
