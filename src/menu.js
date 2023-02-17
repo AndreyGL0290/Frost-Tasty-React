afe5df1cf435c2967816ad993f0352c34e4cc965
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-02-17 23:37:08 +0400

    Added images

--- src/menu.js
+++ src/menu.js
@@ -25,6 +25,13 @@ const menu = {
             imagePath: "/images/products/resized/Пельмени.png",
             info: "Состав:\nМясо курицы,Лук,Вода,Соль,Специи",
             price: 18
+        },
+
+        Content4:
+        {
+            name: "Пельмени Острые с зеленью",
+            imagePath: "/images/products/resized/Пельмени.png",
+            price: 23
         }
     },
 
@@ -35,6 +42,7 @@ const menu = {
         Content1:
         {
             name: "Вареники Картофель",
+            imagePath: "/images/products/resized/Вареники.png",
             info: "Состав:\nТесто для вареников,Картофель,Масло из коровьев сливок,Яйцо,Соль",
             price: 11
         },
@@ -42,6 +50,7 @@ const menu = {
         Content2:
         {
             name: "Вареники Картофель & Грибы",
+            imagePath: "/images/products/resized/Вареники.png",
             info: "Состав:\nТесто,Картофель,Шампиньоны,Лук,Масло подсолнечное,Соль,Специи",
             price: 15
         },
@@ -49,6 +58,7 @@ const menu = {
         Content3:
         {
             name: "Вареники Мясо & Капуста",
+            imagePath: "/images/products/resized/Вареники.png",
             info: "Состав:\nТесто,Фарш свино-говяжий,Капуста белокачанная,Лук,Соль,Специи",
             price: 18
         },
@@ -56,6 +66,7 @@ const menu = {
         Content4:
         {
             name: "Вареники Капуста & Шпик",
+            imagePath: "/images/products/resized/Вареники.png",
             info: "Состав:\nТесто,Капуста квашеная,Шпик свинной,Лук,Соль,Специи",
             price: 15
         },
@@ -63,6 +74,14 @@ const menu = {
         Content5:
         {
             name: "Вареники Сыр имеретинский",
+            imagePath: "/images/products/resized/Вареники.png",
+            info: "Состав:\nТесто,Сыр имеретинский,Яйцо,Соль,Специи",
+            price: 18
+        },
+        Content6:
+        {
+            name: "Вареники с творогом",
+            imagePath: "/images/products/resized/Вареники.png",
             info: "Состав:\nТесто,Сыр имеретинский,Яйцо,Соль,Специи",
             price: 16
         }
@@ -75,6 +94,7 @@ const menu = {
         Content1:
         {
             name: "Котлеты Куриные",
+            imagePath: "/images/products/resized/Котлеты, фрикадельки.jpg",
             info: "Состав:\nФиле курицы,Лук,Соль,Перец,Мука,Яйцо куриное,Сухари панировочные из багета",
             price: 25
         },
@@ -82,6 +102,7 @@ const menu = {
         Content2:
         {
             name: "Котлеты Домашние",
+            imagePath: "/images/products/resized/Котлеты, фрикадельки.jpg",
             info: "Состав:\nСвинина,Говядина,Хлеб белый,Соль специи,Мука для панировки",
             price: 25
         },
@@ -89,6 +110,7 @@ const menu = {
         Content3:
         {
             name: "Фрикадельки Куриные",
+            imagePath: "/images/products/resized/Котлеты, фрикадельки.jpg",
             info: "Состав:\nМясо курицы,Лук,Зелень,Соль,Специи",
             price: 25
         },
@@ -96,6 +118,7 @@ const menu = {
         Content4:
         {
             name: "Фрикадельки Свинина & Говядины",
+            imagePath: "/images/products/resized/Котлеты, фрикадельки.jpg",
             info: "Состав:\nСвинина,Говядина,Лук,Зелень,Соль,Специи",
             price: 25
         },
@@ -103,6 +126,7 @@ const menu = {
         Content5:
         {
             name: "Шницель куриный панированный в багете",
+            imagePath: "/images/products/resized/Шницель.jpg",
             info: "Состав:\nФиле курицы,Яйцо,Соль,Специи,Мука,Сухари панировочные из багета",
             price: 25
         }
@@ -115,6 +139,7 @@ const menu = {
         Content1:
         {
             name: "Блины фаршированные мясом",
+            imagePath: "/images/products/resized/Блины фаршированные.jpg",
             info: "Состав:\nБлины,Свинина,Говядина,Лук,Масло растительное,Соль,Специи",
             price: 25
         },
@@ -122,6 +147,7 @@ const menu = {
         Content2:
         {
             name: "Блины фаршированные творогом",
+            imagePath: "/images/products/resized/Блины с творогом.jpg",
             info: "Состав:\nБлины,Творог из коровьего молока,Сахар ванильный,Сахар,Соль",
             price: 18
         },
@@ -129,6 +155,7 @@ const menu = {
         Content3:
         {
             name: "Творожные сырники",
+            imagePath: "/images/products/resized/Сырники.jpg",
             info: "Состав:\nТворог из коровьего молока,Яйцо,Соль,Сахар,Сахар ванильный,Мука",
             price: 16
         }
@@ -141,6 +168,7 @@ const menu = {
         Content1:
         {
             name: "Салат 'Оливье'",
+            imagePath: "/images/products/resized/Оливье.jpg",
             info: "Состав:\nКартофель,Морковь,Яйцо,Колбаса вареная,Огурец соленый,Лук",
             price: 30
         },
@@ -148,6 +176,7 @@ const menu = {
         Content2:
         {
             name: "Салат 'Министерский'",
+            imagePath: "/images/products/resized/Министерский.jpg",
             info: "Состав:\nГовядина,Лук,Масло растительное,Яйцо,Огурец соленый соль",
             price: 40
         },
@@ -155,6 +184,7 @@ const menu = {
         Content3:
         {
             name: "Салат 'Сельдь под шубой'",
+            imagePath: "/images/products/resized/Сельдь под шубой.jpg",
             info: "Состав:\nСельдь соленая,Лук,Картофель,Яйцо,Морковь,Свекла,Майонез",
             price: 30
         },
@@ -162,6 +192,7 @@ const menu = {
         Content4:
         {
             name: "Салат с крабовыми палочками",
+            imagePath: "/images/products/resized/Салат с крабовыми палочками.png",
             info: "Состав:\nКрабовые палочки,Яйцо,Кукуруза консервированая",
             price: 30
         },
@@ -169,6 +200,7 @@ const menu = {
         Content5: 
         {
             name: "Рулет с сайрой",
+            imagePath: "/images/products/resized/Рулет сайра_тунец.jpg",
             info: "Состав:\nЛаваш,Сайра консервированная,Яйцо,Сыр твердый,Майонез,Лук зеленый",
             price: 40
         },
@@ -176,6 +208,7 @@ const menu = {
         Content6:
         {
             name: "Рулет с тунцом",
+            imagePath: "/images/products/resized/Рулет сайра_тунец.jpg",
             info: "Состав:\nЛаваш,Тунец консервированный,Яйцо,Сыр твердый,Майонез,Лук зеленый",
             price: 40
         },
@@ -183,26 +216,20 @@ const menu = {
         Content7:
         {
             name: "Рулет с твердым сыром и чесноком",
+            imagePath: "/images/products/resized/Рулет с чесноком.jpg",
             info: "Состав:\nЛаваш,Сыр твердый,Яйцо,Чеснок,Лук зеленый,Майонез",
             price: 40
-        },
-        
-        Content8:
-        {
-            name: "Набор из 3 видов салатов и закуски",
-            info: "Состав:\nСалат 'Оливье',Салат 'Сельдь под шубой',Салат 'С крабовыми палочками'",
-            price: 59,
-            postfix: '₾'
         }
     },
 
     Category6:
     {
         name: "Пробные сеты",
-        imagePath: "/images/products/resized/пробный сет_1.png",
+        imagePath: "/images/products/resized/Пробный сет undefined.jpg",
         Content1:
         {
             name: "Пробный сет №1",
+            imagePath: "/images/products/resized/Сет№1.png",
             info: "Состав:\nПельмени Свинина & Говядина, 300г,Пельмени Говядина, 300г,Вареники Картофель, 300г,Вареники Мясо & Капуста, 300г,Котлеты Домашние, 4 шт.,Шницель куриный панированный в багете, 2 шт.,Блины фаршированные творогом, 4 шт.",
             price: 49,
             postfix: '₾'
@@ -211,6 +238,7 @@ const menu = {
         Content2:
         {
             name: "Пробный сет №2",
+            imagePath: "/images/products/resized/Сет№2.png",
             info: "Состав:\nПельмени Свинина & Говядина, 300г,Пельмени Курица, 300г,Вареники Картофель & Грибы, 300г,Вареники Сыр имеретинский, 300г,Котлеты Куриные, 4 шт.,Блины фаршированны мясом, 4 шт.,Сырники, 4 шт.",
             price: 49,
             postfix: '₾'
