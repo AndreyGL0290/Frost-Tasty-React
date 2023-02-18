a3b8855b4399bfaab5278f99ac9552ee37a298b7
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-02-18 18:02:27 +0400

    Bug fix: placeholders z-index doesnt overlay images in fullscreen

--- src/routes/Image.js
+++ src/routes/Image.js
@@ -18,6 +18,12 @@ const Image = props => {
             style={!load ? { display: 'none' } : {}}
             onLoad={() => setLoad(true)}
             onClick={(event) => {
+              document.addEventListener('hashchange', (event) => {
+                event.preventDefault()
+                document.getElementById('image-loader').style.display = 'none'
+                document.getElementById('image-loader').style.backgroundImage = 'none'
+                document.getElementsByTagName('body')[0].style.overflow = 'auto'
+              })
               document.getElementById('image-loader').style.display = 'block' 
               document.getElementById('image-loader').style.backgroundImage = 'url(' + event.target.src + ')'
               document.getElementsByTagName('body')[0].style.overflow = 'hidden'
